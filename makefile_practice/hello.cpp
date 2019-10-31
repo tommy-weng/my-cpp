7e2469e18ed2545946696a309552a8c262e3b9d5
sweng <sweng@nokia-sbell.com>
2019-10-31 10:17:52 +0800

    for makefile practice

---
+++ makefile_practice/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void print_h()
+{
+	cout << "Hello T_T" << endl;
+}
---
+++ makefile_practice/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hello.hpp"
+
+using namespace std;
+
+int main()
+{
+	print_h();
+    cout << "hello main" << endl;
+    return 0;
+}
