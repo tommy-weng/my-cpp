216b71a00c962eb70ccf393f9494c311ac2b669c
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2020-12-03 18:05:26 +0800

    Add test fixture for TestMinFunds.cpp

--- ccc2020_69015657/test/TestMinFunds.cpp
+++ ccc2020_69015657/test/TestMinFunds.cpp
@@ -2,44 +2,55 @@
 #define private public
 #include "../src/minFunds/MinFunds.hpp"
 
-TEST(MinFunds, calcMinFunds) {
+class MinFundsTest : public ::testing::Test
+{
+public:
     using Cities = vector<array<comm::City, MAX_CITIES>>;
     using Weeks = vector<comm::Week>;
     using Pool = comm::Pool;
+    void SetUp() override
+    {
+        array<comm::City, MAX_CITIES> city;
+        city[0].config().maxR_ = 50;
+        city[0].config().smLoad_ = 10;
+        city[0].config().disLoad_ = 20;
+        city[0].config().otherLoad_ = 30;
+        cities.push_back(move(city));
 
-    array<comm::City, MAX_CITIES> city;
-    city[0].config().maxR_ = 50;
-    city[0].config().smLoad_ = 10;
-    city[0].config().disLoad_ = 20;
-    city[0].config().otherLoad_ = 30;
-    Cities cities{city};
-    comm::Week week;
-    week.sumOfMaxR() = 50;
-    Weeks weeks{week};
-    Pool pool;
-    pool.poolR() = 100;
-    pool.poolMinRate() = 40;
-    minFunds::MinFunds minFunds(cities, weeks, pool);
+        comm::Week week;
+        week.sumOfMaxR() = 50;
+        weeks.push_back(move(week));
+
+        pool.poolR() = 100;
+        pool.poolMinRate() = 40;
+    }
+
+    void TearDown() override
+    {
+
+    }
+
+protected:
+    Cities cities{};
+    Weeks weeks{};
+    Pool pool{};
+    minFunds::MinFunds minFunds{cities, weeks, pool};
+};
+
+
+TEST_F(MinFundsTest, calcMinFunds)
+{
     ASSERT_EQ(40, minFunds.calcMinFunds(0, 0)); 
 }
 
-TEST(MinFunds, calcMinFunds_with_Decimals_roundUp) {
-    using Cities = vector<array<comm::City, MAX_CITIES>>;
-    using Weeks = vector<comm::Week>;
-    using Pool = comm::Pool;
-
-    array<comm::City, MAX_CITIES> city;
-    city[0].config().maxR_ = 50;
-    city[0].config().smLoad_ = 10;
-    city[0].config().disLoad_ = 20;
-    city[0].config().otherLoad_ = 30;
-    Cities cities{city};
-    comm::Week week;
-    week.sumOfMaxR() = city[0].config().maxR_;
-    Weeks weeks{week};
-    Pool pool;
+TEST_F(MinFundsTest, calcMinFunds_with_Decimals_roundUp)
+{
     pool.poolR() = 101;
-    pool.poolMinRate() = 40;
-    minFunds::MinFunds minFunds(cities, weeks, pool);
     ASSERT_EQ(41, minFunds.calcMinFunds(0, 0)); 
+}
+
+TEST_F(MinFundsTest, calcMinFundsMultiCityes)
+{
+    minFunds.calcMinFunds(0);
+    ASSERT_EQ(20, cities[0][0].minFundsData().minR_); 
 }
