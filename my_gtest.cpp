a53e3386977236f075de66ca2716934319c143bb
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2021-01-06 13:39:12 +0800

    Update my_gtest.cpp

--- my_gtest.cpp
+++ my_gtest.cpp
@@ -1,3 +1,5 @@
+// g++ my_gtest.cpp -lgtest -lpthread
+
 #include <gtest/gtest.h>
 
 int add(int a, int b)
@@ -5,9 +7,14 @@ int add(int a, int b)
     return a + b;
 }
 
-TEST(test_suite_name, test_name)
+TEST(test_add, add_1_2_eq_3)
 {
     EXPECT_EQ(3, add(1, 2));
+}
+
+TEST(test_add, add_2_3_eq_5)
+{
+    EXPECT_EQ(5, add(2, 3));
 }
 
 int main(int argc, char** argv)
