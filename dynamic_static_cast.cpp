1fc6f2b6f76e50b117679bc517b5d54e78615cd8
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2021-06-03 19:49:49 +0800

    add dynamic_static_cast.cpp

---
+++ dynamic_static_cast.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+class B
+{
+public:
+    virtual void base()
+    {
+        cout << "In base: " << valB << endl;
+    }
+private:
+    int valB{1};
+};
+
+class D : public B
+{
+public:
+    void derived()
+    {
+        cout << "In derived: " << valD << endl;
+    }
+private:
+    int valD{2};
+};
+
+void static_c(B* pb)
+{
+    D* pd1 = static_cast<D*>(pb);
+    if (pd1)
+    {
+        pd1->derived();
+    }
+    else
+    {
+        cout << "null ptr" << endl;
+    }
+}
+
+void dynamic_c(B* pb)
+{
+    D* pd2 = dynamic_cast<D*>(pb);
+    if (pd2)
+    {
+        pd2->derived();
+    }
+    else
+    {
+        cout << "null ptr" << endl;
+    }
+}
+
+void foo(B* pb)
+{
+    static_c(pb);
+    dynamic_c(pb);
+}
+
+int main()
+{
+    foo(new B{});
+    foo(new D{});
+
+    return 0;
+}   
+
