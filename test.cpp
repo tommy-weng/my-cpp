0fc1a0680f4d348c0790326dfa7f6cc5fcb488d1
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2021-07-08 14:04:53 +0800

    modify test.cpp

--- test.cpp
+++ test.cpp
@@ -5,17 +5,20 @@
 using std::cout;
 using std::endl;
 
+void version()__attribute__((constructor));
+void version()
+{
+    cout << "Version <--" << __VERSION__ << "--> " << "Cplusplus <--" << __cplusplus << "-->" << endl;
+}
+
 int main()
 {
-    cout << "Version: <--" << __VERSION__ << "--> " << endl;
-    cout << "Cplusplus: <--" << __cplusplus << "-->" << endl;
-
     std::vector<int> v{ 1, 2, 3 };
     v.emplace_back(4);
 
-    for_each(v.begin(), v.end(), [](int &i){
+    for_each(v.begin(), v.end(), [](auto &i){
         cout << i << endl;
-        });
+    });
 
     return 0;
 }   
