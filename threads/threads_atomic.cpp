6322a76df3c0b50c660f2b17f7be0d22466cd71f
Tommy <shiqiang_weng@163.com>
2022-07-17 16:04:15 +0800

    classify template and threads

---
+++ threads/threads_atomic.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+using std::cout;
+using std::endl;
+
+std::atomic<bool> ready{false};
+
+std::atomic<int> sum{0};
+// int sum{0};
+
+std::mutex mtx{};
+
+void foo(int id)
+{
+    cout << "Read thread # " << id << endl;
+    while(not ready){std::this_thread::yield();}
+    for (int i{0}; i < 10000; i++)
+    {
+        //std::lock_guard<std::mutex> lkg{mtx};
+        sum--;
+        cout << "Read sum: " << sum << endl;
+        //std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+void bar(int id)
+{
+    cout << "Write thread # " << id << endl;
+    while(not ready){std::this_thread::yield();}
+    for (int i{0}; i < 10000; i++)
+    {
+        // std::unique_lock<std::mutex> lkg{mtx};
+        sum++;
+        cout << "Write sum:" << sum << endl;
+        //std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
+int main()
+{
+    std::thread th1(foo, 10);
+    std::thread th2(bar, 11);
+
+    ready = true;
+    th1.join();
+    th2.join();
+    return 0;
+}
