a93b3ff0377baaf68437d771c034ba4bb248ea96
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2023-02-01 16:07:52 +0800

    Update WeakPtr.cpp

--- c++11/WeakPtr.cpp
+++ c++11/WeakPtr.cpp
@@ -29,6 +29,7 @@ public:
 int main() {
 
     std::weak_ptr<A> wa;
+    std::weak_ptr<B> wb;
     {
         std::shared_ptr<A> aa = std::make_shared<A>();
         std::shared_ptr<B> bb = std::make_shared<B>();
@@ -36,11 +37,14 @@ int main() {
         bb->pa = aa;
         //bb->pa.reset();
         
-        wa = bb->pa;
+        wa = aa;
+        wb = bb;
         cout << wa.use_count() << endl;
+        cout << wb.use_count() << endl;
     }
     
     cout << wa.use_count() << endl;
+    cout << wb.use_count() << endl;
 
   return 0;
 }
